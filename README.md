735b511c71ae0218b0f3fd61cbf84e7cff0be1f0
Silvio Waschina <s.waschina@nutrinf.uni-kiel.de>
2022-05-19 14:32:03 +0200

    Final i-dots for submission

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # chem_shift_imaging_NMR
+
+This repository contains the point-wise binned NMR spectra as well as the data analysis R-code for the study "Chemical shift imaging by NMR metabolomics using in tube extraction and slice selection." by Silvio Waschina (Kiel University), Karsten Seeger (University of Lübeck).
+
+## Dependencies
+
+- ***R*** (version >4.1.2)
+- R-packages
+  - `data.table` (v. 1.14.2)
+  - `speaq` (v. 2.6.1)
+  - `ggplot2` (v. 3.3.5)
+  - `ggrepel` (v. 0.9.1)
+  - `bit64` (v. 4.0.5)
+  - `egg` (v. 0.4.5)
+  - `missForest` (v. 1.4)
+
+## Repository structure
+
+- directory `analysis/va/scripts/` - contains all R-scripts
+- directory `analysis/va/plots/` - should be created if not already there. Plots in PDF format will be saved here.
+- directory `data/clean/` contains the sample meta information (e.g. coordinates of samples)
+- directory `data/raw/` contains the actual data (point-wise binned spectra). Files are gzipped plain text files. 
+
+## Data analysis
+
+The data analysis is performed using R. To re-run the analysis, simply run the following commands from a bash terminal within the project directory.
+
+##### Bear-shaped ham
+
+```bash
+Rscript analysis/v1/baerchi_01.R
+```
+
+##### Bierschinken
+
+```bash
+Rscript analysis/v1/beerschi_01.R
+```
+
--- analysis/v1/scripts/beerschi_01.R
+++ analysis/v1/scripts/beerschi_01.R
@@ -23,7 +23,7 @@ beerschi$info[rack == 1 & pos.x == "F" & pos.y == 4, color2 := "g"]
 beerschi$info[rack == 1 & pos.x == "D" & pos.y == 4, color2 := "g"]
 beerschi$info[color2 == "wr", color2 := "r"]
 beerschi$info[, newID := paste0("beer_",1:.N)]
-tmp <- fread("data/raw/bierschinken_pointwise_all_v2")
+tmp <- fread("data/raw/bierschinken_pointwise_all_v2.gz")
 tmp[[1]] <- str_match(tmp[[1]],"Slice-foodomics-Bierschinken-600cryo1_\\s*([0-9]{4}?)\\s*_1$")[,2]
 tmp_names <- tmp[[1]]
 beerschi$spectra <- as.matrix(tmp[,-1])
@@ -66,160 +66,6 @@ pairs(beer_H$PCA$scores[,2:6],
 pairs(beer_G$PCA$scores[,2:6],
       main = "Bierschinken (G)", col = as.factor(beerschi$info$color2))
 dev.off()
-
-# # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #
-# # Combined PCA axis for L+H #
-# # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #
-# beer_HL <- list()
-# beer_HL$color <- cbind(beer_H$color[,.(sampleH = sample, color)],
-#                        beer_L$color[,.(sampleL = sample)],
-#                        beerschi$info[,.(newID,rack,color2)])
-# beer_HL$filled <- rbind(merge(beer_H$filled, beer_HL$color[,.(sample = sampleH,
-#                                                               newID)],
-#                               by = "sample"),
-#                         merge(beer_L$filled, beer_HL$color[,.(sample = sampleL,
-#                                                               newID)],
-#                               by = "sample"))
-# beer_HL$filled$mode <- c(rep("H",nrow(beer_H$filled)),
-#                          rep("L",nrow(beer_L$filled)))
-# beer_HL$filled[, peakIndexMode := paste0(mode,"-", peakIndex)]
-# tmpH <- beer_H$feat.tab; colnames(tmpH) <- paste0("H-",colnames(tmpH))
-# tmpL <- beer_L$feat.tab; colnames(tmpL) <- paste0("L-",colnames(tmpL))
-# beer_HL$feat.tab <- cbind(tmpH,tmpL)
-# rm(tmpH); rm(tmpL)
-# tmp_peaks.features <- dcast(beer_HL$filled, newID ~ peakIndexMode,
-#                             value.var = "peakValue", fill = 0)
-# setkey(tmp_peaks.features, "newID")
-# tmp_peaks.features <- tmp_peaks.features[beer_HL$color$newID]
-# tmp_peaks.features <- as.matrix(tmp_peaks.features[,-1])
-# rownames(tmp_peaks.features) <- beer_HL$color$newID
-# # hier evtl features rausfiltern, die selten >LOD sind
-# tmp_peaks.features <- tmp_peaks.features[,apply(tmp_peaks.features,
-#                                                 2,
-#                                                 function(x) sum(x==0)) <= nrow(tmp_peaks.features) * 0.025]
-# # imputation with kNN (only features, which occur in at least 97.5% of samples)
-# tmp_peaks.features[which(tmp_peaks.features == 0, arr.ind = TRUE)] <- NA
-# tmp_peaks.features <- t(bnstruct::knn.impute(t(tmp_peaks.features)))
-#
-# # exclude features within specific ranges
-# keep_feat <- beer_HL$filled[!(ppm < 0.4 | (ppm > 4.41 & ppm < 6)), unique(peakIndexMode)]
-# keep_feat <- keep_feat[keep_feat %in% colnames(tmp_peaks.features)]
-# tmp_ft.tab.scaled <- speaq::SCANT(data.matrix = tmp_peaks.features[,keep_feat],
-#                                   type = c("center","unit"))
-# # the actual PCA
-# ft.pca <- prcomp(tmp_ft.tab.scaled, scale. = FALSE, center = FALSE)
-# dt.pca <- cbind(data.table(newID = rownames(tmp_ft.tab.scaled)),
-#                 ft.pca$x)
-# dt.pca <- merge(dt.pca, beer_HL$color)
-# dt.pca.loading <- cbind(data.table(feat = rownames(ft.pca$rotation)),
-#                         ft.pca$rotation)
-# dt.pca.loading <- merge(dt.pca.loading, beer_HL$filled[,.(ppm = median(ppm),
-#                                                           peakVal = median(peakValue)),
-#                                                        by = peakIndexMode],
-#                         by.x = "feat", by.y = "peakIndexMode")
-# dt.pca.loading[, scale := ft.pca$scale[feat]]
-# importance.pca <- summary(ft.pca)$importance
-#
-# pairs(dt.pca[,2:6],
-#       col = as.factor(dt.pca$color), main = "Bierschinken (HL)")
-# pairs(dt.pca[,2:6],
-#       col = as.factor(dt.pca$rack), main = "Bierschinken (HL)")
-#
-# plot_pca_feat <- function(PC_OI, legend.position = "none") {
-#   # identify top n features with max sqrt(axis1^2 + axis2^2) loadings
-#   n <- 10
-#   dt.pca.loading[order(abs(get(PC_OI[1])), decreasing = T)][1:n, feat]
-#   # relfeats <- c(dt.pca.loading[order(abs(get(PC_OI[1])), decreasing = T)][1:n, feat],
-#   #               dt.pca.loading[order(abs(get(PC_OI[2])), decreasing = T)][1:n, feat])
-#
-#   relfeats <- dt.pca.loading[order(sqrt((get(PC_OI[1])*scale)^2 + (get(PC_OI[2])*scale)^2), decreasing = T)][1:n, feat]
-#
-#   p_scores <- ggplot(dt.pca, aes(get(PC_OI[1]), get(PC_OI[2]), fill = color2)) +
-#     geom_point(shape = 21) +
-#     scale_fill_manual(values = beerschi_colors2) +
-#     labs(x = paste0(PC_OI[1]," (",
-#                     round(100*importance.pca["Proportion of Variance",PC_OI[1]]),"%)"),
-#          y = paste0(PC_OI[2]," (",
-#                     round(100*importance.pca["Proportion of Variance",PC_OI[2]]),"%)"),
-#          fill = "Color of sample") +
-#     theme_bw() +
-#     theme(panel.grid = element_blank(),
-#           axis.text = element_text(color = "black"),
-#           legend.position = legend.position)
-#
-#   scaling_fac <- 1
-#   tmpmat <- as.matrix(dt.pca.loading[feat %in% relfeats,.(get(PC_OI[1]),get(PC_OI[2]))])
-#   for(isf in 2:50) {
-#     tmptmpmat <- tmpmat * isf
-#     if(min(tmptmpmat[,1]) < min(dt.pca[[PC_OI[1]]]) |
-#        max(tmptmpmat[,1]) > max(dt.pca[[PC_OI[1]]]) |
-#        min(tmptmpmat[,2]) < min(dt.pca[[PC_OI[2]]]) |
-#        max(tmptmpmat[,2]) > max(dt.pca[[PC_OI[2]]]))
-#       break
-#     scaling_fac <- isf
-#   }
-#
-#   p_loading <- ggplot(dt.pca.loading[feat %in% relfeats],
-#                       aes(xend = get(PC_OI[1]) * scaling_fac,
-#                           yend = get(PC_OI[2]) * scaling_fac,
-#                           col = ifelse(grepl("H", feat),"H","L"))) +
-#     geom_segment(x = 0, y = 0, alpha = 0.4)  +
-#     geom_text_repel(aes(x=get(PC_OI[1]) * scaling_fac,
-#                         y=get(PC_OI[2]) * scaling_fac,
-#                         label = round(ppm, digits = 2)),
-#                     max.overlaps = 100, show.legend = FALSE,
-#                     min.segment.length = 100) +
-#     scale_x_continuous(limits = c(min(dt.pca[[PC_OI[1]]]),max(dt.pca[[PC_OI[1]]]))) +
-#     scale_y_continuous(limits = c(min(dt.pca[[PC_OI[2]]]),max(dt.pca[[PC_OI[2]]]))) +
-#     scale_color_manual(values = c("#d01c8b","#4dac26")) +
-#     labs(x = paste0(PC_OI[1]," (",
-#                     round(100*importance.pca["Proportion of Variance",PC_OI[1]]),"%)"),
-#          y = paste0(PC_OI[2]," (",
-#                     round(100*importance.pca["Proportion of Variance",PC_OI[2]]),"%)"),
-#          col = "Phase") +
-#     theme_bw() +
-#     theme(panel.grid = element_blank(),
-#           axis.text = element_text(color = "black"),
-#           legend.position = legend.position)
-#
-#   p_out <- ggpubr::ggarrange(p_scores, p_loading, nrow = 1)
-#   return(p_out)
-# }
-#
-# pcaHL_12 <- plot_pca_feat(c("PC1","PC2"), legend.position = "right")
-# pcaHL_13 <- plot_pca_feat(c("PC1","PC3"), legend.position = "bottom")
-# pcaHL_23 <- plot_pca_feat(c("PC2","PC3"), legend.position = "bottom")
-#
-#
-# # ~ ~ ~ ~ ~ ~ #
-# # Beer plot   #
-# # ~ ~ ~ ~ ~ ~ #
-# dt_scaled <- data.table(as.table(tmp_ft.tab.scaled))
-# setnames(dt_scaled, c("newID","feat","signal"))
-# dt_scaled <- merge(dt_scaled, beerschi$info, by = "newID")
-# dt_scaled[, pos.y.new := pos.y]
-# dt_scaled[rack == 2, pos.y.new := pos.y.new + 12]
-# dt_scaled[, pos.x.new := pos.x]
-# dt_scaled[rack == 3 & pos.x == "A", pos.x.new := "I"]
-# dt_scaled[rack == 3 & pos.x == "B", pos.x.new := "J"]
-#
-# ggplot(dt_scaled[feat == "H-14100"], aes(x = pos.y.new, y = pos.x.new,
-#                                          fill = signal)) +
-#   geom_tile()
-#
-# dt_pca_beer <- copy(dt.pca[,1:10])
-# dt_pca_beer <- merge(dt_pca_beer, beerschi$info, by = "newID")
-# dt_pca_beer[, pos.y.new := pos.y]
-# dt_pca_beer[rack == 2, pos.y.new := pos.y.new + 12]
-# dt_pca_beer[, pos.x.new := pos.x]
-# dt_pca_beer[rack == 3 & pos.x == "A", pos.x.new := "I"]
-# dt_pca_beer[rack == 3 & pos.x == "B", pos.x.new := "J"]
-#
-#
-# ggplot(dt_pca_beer, aes(x = pos.y.new, y = pos.x.new,
-#                         fill = PC2)) +
-#   geom_tile() +
-#   scale_y_discrete(limits = rev)
 
 
 # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #
@@ -648,17 +494,17 @@ ggsave("analysis/v1/plots/bierschinken_G_PC1_PC2.pdf", plot = p_dist_pc12,
 
 
 
-pairs(beer_L$PCA$scores[,2:6],
-      col = as.factor(beer_L$PCA$scores$color), main = "Bierschinken (L)")
-pairs(beer_H$PCA$scores[,2:6],
-      col = as.factor(beer_H$PCA$scores$color), main = "Bierschinken (H)")
-pairs(beer_G$PCA$scores[,2:6],
-      col = as.factor(beer_G$PCA$scores$color), main = "Bierschinken (G)")
-dev.off()
-
-
-ROI.ppm <- 6.1
-roiWidth.ppm <- 0.1
-
-ROIplot_SW(beer_H, ROI = ROI.ppm, ROI.width = roiWidth.ppm,
-           color.code = MetBrewer::met.brewer("Isfahan2",7))
+# pairs(beer_L$PCA$scores[,2:6],
+#       col = as.factor(beer_L$PCA$scores$color), main = "Bierschinken (L)")
+# pairs(beer_H$PCA$scores[,2:6],
+#       col = as.factor(beer_H$PCA$scores$color), main = "Bierschinken (H)")
+# pairs(beer_G$PCA$scores[,2:6],
+#       col = as.factor(beer_G$PCA$scores$color), main = "Bierschinken (G)")
+# dev.off()
+# 
+# 
+# ROI.ppm <- 6.1
+# roiWidth.ppm <- 0.1
+# 
+# ROIplot_SW(beer_H, ROI = ROI.ppm, ROI.width = roiWidth.ppm,
+#            color.code = MetBrewer::met.brewer("Isfahan2",7))
